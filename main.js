ed7dd49da54066cd348a4948e5f9ad397bbc1121
Parthip <projectcs34@gmail.com>
2025-07-06 19:46:26 +0530

    Add display new card functionality

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const myLib = [];
-function Book(){
-    this.id = crypto.randomUUID;
+function Book(title,author,read){
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.read = read;
@@ -32,13 +32,61 @@ function toggleReadStatus(id){
         displayBooks();
     }
 }
+
+const cardContainer = document.querySelector('.cards')
 function displayBooks(){
+    myLib.forEach((book) => {
+        cardContainer.innerHTML = '';
+
+        const div = document.createElement('div');
+        div.classList.add('card');
+
+        const title = document.createElement('div')
+        title.textContent = book.title
+        title.classList.add('name')
+        div.appendChild(title)
+    
+        const author = document.createElement('div')
+        author.textContent = book.author
+        div.appendChild(author);
+
+        const seen = document.createElement('div')
+        seen.classList.add('seen')
+        const span = document.createElement('span')
+        span.textContent = 'Read?'
+        seen.appendChild(span)
+        const input = document.createElement('input')
+        input.type = 'checkbox';
+        input.checked = book.read;
+        input.addEventListener('change' ,()=>toggleReadStatus(book.id))
+        seen.append(span,input);
+        div.appendChild(seen);
+
+        const close = document.createElement('button')
+        close.classList.add('remove-btn')
+        close.textContent = '❌';
+        close.addEventListener('click' , ()=> removeBook(book.id));
+        div.appendChild(close)
+
+        cardContainer.appendChild(div);
+    })
 
 }
 
+formContainer = document.querySelector('.form-container');
+form =document.querySelector('.add-form')
+saveBtn = document.querySelector('.submit');
 addBtn = document.querySelector(".add-book");
 clsBtn = document.querySelector('.close-btn');
-formContainer = document.querySelector('.form-container');
-
+bName = document.querySelector('#name')
+authName = document.querySelector('#auth-name')
+readCheck = document.querySelector('#read-check')
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const name = bName.value
+    const author = authName.value
+    const read = readCheck.checked
+    addBook(name,author,read)
+})
 addBtn.addEventListener("click",() => formContainer.style.display = 'block')
 clsBtn.addEventListener('click',()=>  formContainer.style.display = 'none')
